3029ebf4a026a0c5450abc89aa097dd1f57818b1
191 <kawauso.1288@gmail.com>
2025-08-16 15:05:56 +0900

    add example.py

---
+++ example.py
@@ -0,0 +1,30 @@
+
+print('Hello World')
+
+
+
+def greeting(message):
+    return message 
+greet=greeting("こんにちは")
+print(greet)
+
+
+
+name ="あざらし"
+print("私の名前は"+name+"です")
+
+
+
+
+def greeting(message):
+  return message
+greet=greeting("おはようございます")
+print(greet)
+
+
+def add(a, b):
+    return a + b
+result = add(3, 5)
+print(result)
+
+
